cd9de99676bf5e890a74c493d56768b0330cc2d7
Muhamad Zulfiqor <siapaa249@gmail.com>
2024-09-30 09:19:01 +0700

    ✨ fonts

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
+        fontFamily: 'Poppins',
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
